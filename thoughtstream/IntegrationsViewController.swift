2312bef37b881b1f52d0aeb40b401b649d71d8b3
Jared Smith <jared.rit@gmail.com>
2014-10-04 17:25:55 -0400

    Change naming of controllers, add navigation controller and another section for integrations. May change to list view later.

---
+++ thoughtstream/IntegrationsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  IntegrationsViewController.swift
+//  thoughtstream
+//
+//  Created by Jared Smith on 10/4/14.
+//  Copyright (c) 2014 ThoughtStream. All rights reserved.
+//
+
+import UIKit
+
+class IntegrationsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
